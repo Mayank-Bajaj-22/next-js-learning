6d16815cf4b25327f242e897ba88bccffed03e4e
Mayank Bajaj <mayankbajaj672@gmail.com>
2026-02-11 22:42:45 +0530

    dynamic routing in next js

---
+++ Section 2/01_routing-in-next.js/app/page.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        Welcome to Home Page!!
+      </h1>
+
+      <Link href="/about">
+        About
+      </Link>
+
+      <Link href="/services">
+        Services
+      </Link>
+    </>
+  );
+}
